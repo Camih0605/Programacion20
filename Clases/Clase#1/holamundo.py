20e892f303b3564d9c035fde739d1fd7d0a6cedd
camih0506 <herrerab.maria@uces.edu.co>
2020-02-19 12:13:36 -0500

    holamundo

---
+++ Clases/Clase#1/holamundo.py
@@ -0,0 +1,13 @@
+#Codigo primera clase
+nombre = "Camila"
+print (nombre)
+print (type(nombre))
+edad = 18
+print (edad)
+print (type(edad))
+dinero = 9283374748432993.23
+print (dinero)
+print (type(dinero))
+
+
+
